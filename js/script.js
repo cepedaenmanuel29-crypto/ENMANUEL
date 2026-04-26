5331455800c1893a48d843e7980e11b200d0382d
cepedaenmanuel29-crypto <cepedaenmanuel29@gmail.com>
2026-04-26 14:47:05 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,9 @@
-function playAudio() {
+function playAudio(){
 document.getElementById("audio").play();
 }
 
-// scroll effect
-function scrollToSection() {
+function scrollToSection(){
 document.getElementById("mainContent").scrollIntoView({
-behavior: "smooth"
+behavior:"smooth"
 });
 }
-
-// page load
-document.addEventListener("DOMContentLoaded", function() {
-console.log("Website Loaded");
-});
